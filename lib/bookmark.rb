0d1c70f7ac5ebc506c5ac3daa5a2cc1920f49c4e
George Sykes <georgesykes86@gmail.com>
2018-05-10 09:51:39 +0100

    Adds object creation in Bookmark

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,20 +4,35 @@ require_relative './database_connection'
 
 class Bookmark
 
+  def self.create(id:, title:, url:)
+    Bookmark.new(id: id, title: title, url: url)
+  end
+
   def self.all
     result = DatabaseConnection.query("SELECT * FROM bookmarks")
     result.map { |row| row['url'] }
   end
 
-  def self.add(url)
+  def self.add(url, title = nil)
     return false unless valid_url?(url)
-    DatabaseConnection.query("INSERT INTO bookmarks (url) VALUES ('#{url}');")
+    result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, url, title;")
+    create(id: result[0]['id'].to_i, title: result[0]['title'], url: result[0]['url'] )
   end
 
   private
 
   def self.valid_url?(url)
      !!(url =~ /\A#{URI::regexp}\z/)
+  end
+
+  public
+
+  attr_reader :title, :url, :id
+
+  def initialize(id:, url:, title: nil)
+    @id = id
+    @url = url
+    @title = title
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -18,13 +18,31 @@ describe Bookmark do
 
   context '#add' do
     it 'adds a url to the array of bookmarks' do
-      described_class.add('http://thoughtbot.com')
+      described_class.add('http://thoughtbot.com', 'thoughtbot')
       expect(described_class.all).to include 'http://thoughtbot.com'
     end
 
     it 'does not add an incorrect url' do
       described_class.add('not a valid url')
       expect(described_class.all).not_to include 'not a valid url'
+    end
+
+    it 'returns a Bookmark object' do
+      result = described_class.add('http://thoughtbot.com', 'thoughtbot')
+      expect(result.url).to eq "http://thoughtbot.com"
+    end
+  end
+
+  describe '#self.create' do
+    let(:bookmark) { described_class.create(id: 2, url: 'http://bbc.co.uk', title: 'bbc') }
+    it 'creates an instance of Bookmark' do
+      expect(bookmark).to be_an_instance_of(described_class)
+    end
+
+    it 'gives the bookmark a title, url and an id' do
+      expect(bookmark.id).to eq 2
+      expect(bookmark.url).to eq "http://bbc.co.uk"
+      expect(bookmark.title).to eq "bbc"
     end
   end
 
