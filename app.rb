ccabdcbf9e01d173f264843e6fa8baedf15f5645
George Sykes <georgesykes86@gmail.com>
2018-05-10 16:00:12 +0100

    Adds update

--- app.rb
+++ app.rb
@@ -7,6 +7,25 @@ require 'sinatra/flash'
    enable :sessions
    register Sinatra::Flash
 
+   helpers do
+
+     def update_params_handler(params)
+       update_hash = Hash.new
+       params.each do |key, value|
+         id, field = key.split("_")[0].to_i, key.split("_")[1]
+         update_hash[id] ||= {}
+         update_hash[id][field.to_sym] = value
+       end
+       update_hash
+     end
+
+     def update_all(bookmarks)
+       bookmarks.each do |id, value_hash|
+         Bookmark.update(id: id, url: value_hash[:url], title: value_hash[:title])
+       end
+     end
+
+   end
 
    get '/' do
      @bookmarks = Bookmark.all
@@ -30,6 +49,11 @@ require 'sinatra/flash'
 
    get '/update' do
      erb(:update)
+   end
+
+   post '/updated' do
+     update_all(update_params_handler(params))
+     redirect '/'
    end
 
  end
--- spec/features/update_bookmark_spec.rb
+++ spec/features/update_bookmark_spec.rb
@@ -4,8 +4,9 @@ feature 'update bookmark' do
     fill_in 'url', with: 'http://www.amazon.co.uk'
     fill_in 'title', with: 'Amazon'
     click_button 'Add Bookmark'
+    fill_in 'update_title', with: 'Amazon'
     click_button 'Update'
-    find('url').set('http://www.amazon.com')
+    fill_in 'url', with: 'http://www.amazon.com'
     click_button 'Submit'
     expect(page).to have_link(href: 'http://www.amazon.com')
   end
